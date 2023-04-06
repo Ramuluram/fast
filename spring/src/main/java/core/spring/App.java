ec33a59ab6e7a5481eecf5957eac5a9a59fe519e
Ramulu <coderamulu@gmail.com>
2023-04-06 21:27:08 +0530

    first

---
+++ spring/src/main/java/core/spring/App.java
@@ -0,0 +1,14 @@
+package core.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("spring");
+    }
+}
